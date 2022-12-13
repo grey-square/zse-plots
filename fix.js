8693686d394bd8196c5537d6247d27b7bbfc13a0
Dzinaishe Mpini <dzinampini@gmail.com>
2022-12-13 11:12:09 +0200

    ADDED: project files

---
+++ fix.js
@@ -0,0 +1,96 @@
+    if row.split(",")[0] == 'AFDIS':
+        company_position = 1
+    if row.split(",")[0] == 'AFRICAN SUN':
+        company_position = 2
+    if row.split(",")[0] == 'ARISTON':
+        company_position = 4
+    if row.split(",")[0] == 'ART ZDR':
+        company_position = 3
+    if row.split(",")[0] == 'AXIA':
+        company_position = 5
+    if row.split(",")[0] == 'BAT'
+        company_position = 6
+    if row.split(",")[0] == 'CAFCA':
+        company_position = 7
+    if row.split(",")[0] == 'CBZ'
+        company_position = 8
+    if row.split(",")[0] == 'CFI'
+        company_position = 9
+    if row.split(",")[0] == 'DAIRIBORD':
+        company_position = 10
+    if row.split(",")[0] == 'DELTA':
+        company_position = 11
+    if row.split(",")[0] == 'ECOCASH':
+        company_position = 12
+    if row.split(",")[0] == 'ECONET':
+        company_position = 13
+    if row.split(",")[0] == 'EDGARS':
+        company_position = 14
+    if row.split(",")[0] == 'FBCH':
+        company_position = 15
+    if row.split(",")[0] == 'FIDELITY':
+        company_position = 16
+    if row.split(",")[0] == 'FCA/BARCLAYS':
+        company_position = 17
+    if row.split(",")[0] == '(fml)/AFRE':
+        company_position = 18
+    if row.split(",")[0] == 'FMP'
+        company_position = 19
+    if row.split(",")[0] == 'GB HOLDINGS':
+        company_position = 20
+    if row.split(",")[0] == 'GET BUCKS':
+        company_position = 21
+    if row.split(",")[0] == 'HIPPO':
+        company_position = 22
+    if row.split(",")[0] == 'INNSCOR':
+        company_position = 23
+    if row.split(",")[0] == 'LAFARGE CEMENT':
+        company_position = 24
+    if row.split(",")[0] == 'MASH  HLDS':
+        company_position = 25
+    if row.split(",")[0] == 'MASIMBA':
+        company_position = 26
+    if row.split(",")[0] == 'MEIK':
+        company_position = 29
+    if row.split(",")[0] == 'NAMPAK':
+        company_position = 30
+    if row.split(",")[0] == 'NATFOODS':
+        company_position = 31
+    if row.split(",")[0] == 'N T S':
+        company_position = 32
+    if row.split(",")[0] == 'N.M.B':
+        company_position = 33
+    if row.split(",")[0] == 'OK ZIMBABWE':
+        company_position = 34
+    if row.split(",")[0] == 'PROPLASTICS':
+        company_position = 35
+    if row.split(",")[0] == 'R T G':
+        company_position = 36
+    if row.split(",")[0] == 'RIOZIM':
+        company_position = 37
+    if row.split(",")[0] == 'SEED-CO':
+        company_position = 38
+    if row.split(",")[0] == 'SIMBISA':
+        company_position = 39
+    if row.split(",")[0] == 'STAR AFRICA':
+        company_text = 'STAR AFRICA'
+    if row.split(",")[0] == 'TANGANDA':
+        company_position = 41
+    if row.split(",")[0] == 'TRUWORTHS':
+        company_position = 42
+    if row.split(",")[0] == 'T. S. L':
+        company_position = 43
+    if row.split(",")[0] == 'TURNALL':
+        company_position = 44
+    if row.split(",")[0] == 'UNIFREIGHT':
+        company_position = 45
+    if row.split(",")[0] == 'WILLDALE':
+        company_position = 46
+    if row.split(",")[0] == 'ZECO':
+        company_position = 48
+    if row.split(",")[0] == 'ZIMPAPERS':
+        company_position = 50
+    if row.split(",")[0] == 'ZIMPLOW':
+        company_position = 51
+    if row.split(",")[0] == 'ZIMRE':
+        company_position = 52
---
+++ zse-companies.py
@@ -0,0 +1,180 @@
+company_text = ''
+company_position = 0
+
+if company_text == 'African Distillers':
+    company_text = 'AFDIS'
+    company_position = 1
+if company_text == 'African Sun':
+    company_text = 'AFRICAN SUN'
+    company_position = 2
+if company_text == 'Ariston Holdings':
+    company_text = 'ARISTON'
+    company_position = 4
+if company_text == 'ART Corporation':
+    company_text = 'ART ZDR'
+    company_position = 3
+if company_text == 'Astra Industries':
+    company_text = 'Not present at time of study'
+if company_text == 'Axia Corporation':
+    company_text = 'AXIA'
+    company_position = 5
+if company_text == 'Bindura Nickel Corporation':
+    company_text = 'Not present at time of study'
+if company_text == 'Border Timbers':
+    company_text = 'Not present at time of study'
+if company_text == 'Bridgefort Capital':
+    company_text = 'Not present at time of study'
+if company_text == 'British American Tobacco Zimbabwe Zimbabwe':
+    company_text = 'BAT'
+    company_position = 6
+if company_text == 'Cafca':
+    company_text = 'CAFCA'
+    company_position = 7
+if company_text == 'CBZ Holdings':
+    company_text = 'CBZ'
+    company_position = 8
+if company_text == 'CFI Holdings':
+    company_text = 'CFI'
+    company_position = 9
+if company_text == 'COTTCO Holdings':
+    company_text = 'Not present at time of study'
+if company_text == 'Dairibord Holdings':
+    company_text = 'DAIRIBORD'
+    company_position = 10
+if company_text == 'Dawn Properties':
+    company_text = 'Not present at time of study'
+if company_text == 'Delta Corporation':
+    company_text = 'DELTA'
+    company_position = 11
+if company_text == 'EcoCash Holdings Zimbabwe':
+    company_text = 'ECOCASH'
+    company_position = 12
+if company_text == 'Econet Wireless Zimbabwe':
+    company_text = 'ECONET'
+    company_position = 13
+if company_text == 'Edgars Stores':
+    company_text = 'EDGARS'
+    company_position = 14
+if company_text == 'Falcon Gold Zimbabwe':
+    company_text = 'Not present at time of study'
+if company_text == 'FBC Holdings':
+    company_text = 'FBCH'
+    company_position = 15
+if company_text == 'Fidelity Life Assurance':
+    company_text = 'FIDELITY'
+    company_position = 16
+if company_text == 'First Capital Bank':
+    company_text = 'FCA/BARCLAYS'
+    company_position = 17
+if company_text == 'First Mutual Holdings':
+    company_text = '(fml)/AFRE'
+    company_position = 18
+if company_text == 'First Mutual Properties':
+    company_text = 'FMP'
+    company_position = 19
+if company_text == 'General Beltings Holdings':
+    company_text = 'GB HOLDINGS'
+    company_position = 20
+if company_text == 'GetBucks Microfinance Bank':
+    company_text = 'GET BUCKS'
+    company_position = 21
+if company_text == 'Hippo Valley Estates':
+    company_text = 'HIPPO'
+    company_position = 22
+if company_text == 'Hwange Colliery':
+    company_text = 'Not present at time of study'
+if company_text == 'Innscor Africa':
+    company_text = 'INNSCOR'
+    company_position = 23
+if company_text == 'Lafarge Cement Zimbabwe':
+    company_text = 'LAFARGE CEMENT'
+    company_position = 24
+if company_text == 'Mashonaland Holdings':
+    company_text = 'MASH  HLDS'
+    company_position = 25
+if company_text == 'Masimba Holdings':
+    company_text = 'MASIMBA'
+    company_position = 26
+if company_text == 'Bridgefort Capital':
+    company_text = 'Not present at time of study'
+if company_text == 'Meikles':
+    company_text = 'MEIK'
+    company_position = 29
+if company_text == 'Nampak Zimbabwe':
+    company_text = 'NAMPAK'
+    company_position = 30
+if company_text == 'National Foods Holdings':
+    company_text = 'NATFOODS'
+    company_position = 31
+if company_text == 'National Tyre Services':
+    company_text = 'N T S'
+    company_position = 32
+if company_text == 'Nicoz Diamond Insurance':
+    company_text = 'Not present at time of study'
+if company_text == 'NMBZ Holdings':
+    company_text = 'N.M.B'
+    company_position = 33
+if company_text == 'OK Zimbabwe':
+    company_text = 'OK ZIMBABWE'
+    company_position = 34
+if company_text == 'Old Mutual':
+    company_text = 'Not present at time of study'
+if company_text == 'Padenga Holdings':
+    company_text = 'Not present at time of study'
+if company_text == 'Powerspeed Electrical':
+    company_text = 'Not present at time of study'
+if company_text == 'Pretoria Portland Cement':
+    company_text = 'Not present at time of study'
+if company_text == 'Proplastics':
+    company_text = 'PROPLASTICS'
+    company_position = 35
+if company_text == 'Rainbow Tourism Group':
+    company_text = 'R T G'
+    company_position = 36
+if company_text == 'RioZim':
+    company_text = 'RIOZIM'
+    company_position = 37
+if company_text == 'Seed Co':
+    company_text = 'SEED-CO'
+    company_position = 38
+if company_text == 'Simbisa Brands':
+    company_text = 'SIMBISA'
+    company_position = 39
+if company_text == 'Starafricacorporation':
+    company_text = 'STAR AFRICA'
+if company_text == 'Tanganda Tea Company':
+    company_text = 'TANGANDA'
+    company_position = 41
+if company_text == 'Truworths':
+    company_text = 'TRUWORTHS'
+    company_position = 42
+if company_text == 'TSL':
+    company_text = 'T. S. L'
+    company_position = 43
+if company_text == 'Turnall Holdings':
+    company_text = 'TURNALL'
+    company_position = 44
+if company_text == 'Unifreight Africa':
+    company_text = 'UNIFREIGHT'
+    company_position = 45
+if company_text == 'Willdale':
+    company_text = 'WILLDALE'
+    company_position = 46
+if company_text == 'ZB Financial Holdings':
+    company_text = 'Not present at time of study'
+if company_text == 'Zeco Holdings':
+    company_text = 'ZECO'
+    company_position = 48
+if company_text == 'Zimbabwe Newspapers (1980)':
+    company_text = 'ZIMPAPERS'
+    company_position = 50
+if company_text == 'Zimplow Holdings':
+    company_text = 'ZIMPLOW'
+    company_position = 51
+if company_text == 'Zimre Holdings':
+    company_text = 'ZIMRE'
+    company_position = 52
+if company_text == 'Zimre Property Investments':
+    company_text = 'Not present at time of study'
+if company_text == 'ZS':
+    company_text = 'Not present at time of study'
